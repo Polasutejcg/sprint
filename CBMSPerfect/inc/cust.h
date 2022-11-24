6bef1387939356ceba404c9230170e44c1acbebc
Polasutejcg <117061118+Polasutejcg@users.noreply.github.com>
2022-11-24 18:37:38 +0530

    Add files via upload

---
+++ CBMSPerfect/inc/cust.h
@@ -0,0 +1,36 @@
+/***************************************************************************
+*                       MACROS
+***************************************************************************/
+#ifndef CUST_H
+#define CUST_H
+/***************************************************************************
+*                       STRUCTURES
+***************************************************************************/
+
+typedef struct customer
+{
+	int _id;
+	int phone;
+	char name[20];
+	char gender;
+	char cName[20];
+	char cPasswd[20];
+	struct customer *next;
+
+}CUST;
+/***************************************************************************
+*                       LOCAL FUNCTIONS
+***************************************************************************/
+int custMenu();
+CUST* signUpCust(CUST *,int *);
+int signInCust(CUST *);
+void dispCust(CUST *);
+int writeCustDetails(CUST*);
+CUST* loadCustDetails();
+int tokenizeCUST(CUST *, char *);
+
+#endif
+/*END OF MACRO*/
+
+
+
